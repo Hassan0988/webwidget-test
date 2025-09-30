4caf36baa8b1e1e432693720bb026b669afa03ef
hassan0988 <hassan.naveed22D@gmail.com>
2025-09-30 06:17:29 +0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import { resolve } from "path";
 import { fileURLToPath } from "url";
 
 export default defineConfig({
+  server: {
+    allowedHosts: [
+      "julietta-unjewelled-rosily.ngrok-free.dev",
+      "localhost",
+      "127.0.0.1",
+      "*.netlify.app"
+    ]
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)), // Alias for src folder
